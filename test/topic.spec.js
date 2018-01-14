3808e354112b57555134c236e4089bf5235ff53c
Rick Gorman <gormanite@gmail.com>
2018-01-14 17:49:58 -0500

    Add some tests and convert messageBeingProcessed into a hash

--- test/topic.spec.js
+++ test/topic.spec.js
@@ -13,18 +13,22 @@ describe('Topic', () => {
     context('normally', () => {
       let topic = new Topic(name, timeout);
 
-      it('should create empty process queues', () => {
+      it('should create empty process containers', () => {
         assert.notEqual(topic.messagesAwaitingProcessing, undefined);
         assert.notEqual(topic.messagesBeingProcessed, undefined);
         assert.notEqual(topic.messagesUnprocessable, undefined);
       });
 
       it('should initialize a set of processing statistics', () => {
-        assert.ok(false, 'UNIMPLEMENTED');
+        assert.equal(topic.statistics.singleFailures, 0);
+        assert.equal(topic.statistics.multipleFailures, 0);
+        assert.equal(topic.statistics.unprocessableMessages, 0);
+        assert.equal(topic.statistics.averageProcessingTimeMS, 0);
       });
 
       it('should receive a unique id number', () => {
-        assert.ok(false, 'UNIMPLEMENTED');
+        let topic2 = new Topic('whatever', timeout);
+        assert.notEqual(topic.id, topic2.id);
       });
     });
 
@@ -58,10 +62,77 @@ describe('Topic', () => {
   });
 
   describe('#sendMessage()', () => {
+    const topic = new Topic('whatever');
+    const body = 'awesome';
 
+    it('should create a new message with the given body', () => {
+      topic.sendMessage(body);
+      assert.equal(body, topic.messagesAwaitingProcessing._peek().body);
+    });
+
+    it('should place this message at the BACK of the queue', () => {
+      let secondBody = 'great';
+      topic.sendMessage(secondBody);
+      assert.equal(
+        secondBody,
+        topic.messagesAwaitingProcessing._peekRear().body
+      );
+    });
+  });
+
+  describe('#receiveMessage()', () => {
+    context('with messages ready to process', () => {
+      const topic = new Topic('whatever');
+      const message = new Message('foo');
+      let result;
+
+      it('should move one message from the messagesAwaitingProcessing to messagesBeingProcessed queue', () => {
+        assert.equal(0, topic.messagesAwaitingProcessing.length);
+        topic.sendMessage(message);
+        assert.equal(1, topic.messagesAwaitingProcessing.length);
+
+        result = topic.receiveMessage();
+        assert.equal(0, topic.messagesAwaitingProcessing.length);
+        assert.equal(1, Object.keys(topic.messagesBeingProcessed).length);
+      });
+
+      it('should return a JSONAPI-formatted representation of the message', () => {
+        assert.equal(
+          result,
+          {
+            data: [{
+              id: message.id,
+              body: message.body,
+              createdAt: message.createdAt,
+              processAttempts: message.processAttempts,
+            }],
+            relationships: {
+              topic: {
+                data: { id: topic.id }
+              }
+            }
+          }
+        );
+      });
+    });
+
+    context('when the topic has no messages available to process', () => {
+      const topic = new Topic('empty');
+
+      const receiveMessageFromEmptyTopic = () => {
+        topic.receiveMessage();
+      };
+
+      it('should raise an error', () => {
+        assert.throws(receiveMessageFromEmptyTopic, Error);
+      });
+    });
+  });
+
+  describe('#acknowledgeCompletion()', () => {
     context('when given an as-yet-unseen Message object', () => {
       let topic = new Topic('whatever');
-      let message = new Message('foo');
+      let messageBody = new Message('foo');
 
       it('should add that message to the BACK of the wait queue', () => {
         topic.sendMessage(message);
@@ -96,65 +167,13 @@ describe('Topic', () => {
 
       it('should add that message to the unprocessableMessages array', () => {
         assert.equal(0, topic.messagesAwaitingProcessing.length);
-        assert.equal(0, topic.messagesBeingProcessed.length);
+        assert.equal(0, Object.keys(topic.messagesBeingProcessed).length);
         assert.equal(1, topic.messagesUnprocessable.length);
       });
     });
-  });
 
-  describe('#receiveMessage()', () => {
-    context('with messages ready to process', () => {
-      const topic = new Topic('whatever');
-      const message = new Message('foo');
-      let result;
-
-      it('should move one message from the messagesAwaitingProcessing to messagesBeingProcessed queue', () => {
-        assert.equal(0, topic.messagesAwaitingProcessing.length);
-        topic.sendMessage(message);
-        assert.equal(1, topic.messagesAwaitingProcessing.length);
-
-        result = topic.receiveMessage();
-        assert.equal(0, topic.messagesAwaitingProcessing.length);
-        assert.equal(1, topic.messagesBeingProcessed.length);
-      });
-
-      it('should return a JSONAPI-formatted representation of the message', () => {
-        assert.equal(
-          result,
-          {
-            data: [{
-              id: message.id,
-              body: message.body,
-              createdAt: message.createdAt,
-              processAttempts: message.processAttempts,
-            }],
-            relationships: {
-              topic: {
-                data: { id: topic.id }
-              }
-            }
-          }
-        );
-      });
-    });
-
-    context('when the topic has no messages available to process', () => {
-      const topic = new Topic('empty');
-
-      const receiveMessageFromEmptyTopic = () => {
-        topic.receiveMessage();
-      };
-
-      it('should raise an error', () => {
-        assert.throws(receiveMessageFromEmptyTopic, Error);
-      });
-    });
-
-  });
-
-  describe('#acknowledgeCompletion()', () => {
     context('when performed on the first processing of a message', () => {
-      it('should remove that message from the messagesBeingProcessed queue', () => {
+      it('should remove that message from the messagesBeingProcessed hash', () => {
         assert.ok(false, 'UNIMPLEMENTED');
       });
 
