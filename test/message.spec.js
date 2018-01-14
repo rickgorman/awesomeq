d9709a55b18d91233627edbac853a671c3ca1660
Rick Gorman <gormanite@gmail.com>
2018-01-13 23:28:51 -0500

    Add tests for Message

---
+++ test/message.spec.js
@@ -0,0 +1,67 @@
+var assert = require('assert');
+
+import Message from '../lib/message';
+
+describe('Message', () => {
+  describe('#constructor()', () => {
+    context('without a message body', () => {
+      const createMessageWithoutBody = () => {
+        new Message();
+      };
+      it('should raise an error', () => {
+        assert.throws(createMessageWithoutBody, Error);
+      });
+    });
+
+    context('with a valid message body', () => {
+      const body = 'this is totally valid';
+      const message = new Message(body);
+      it('should store the body', () => {
+        assert.equal(body, message.body);
+      });
+
+      it('should initialize the process counter', () => {
+        assert.equal(0, message.processCounter);
+      });
+    });
+  });
+
+  describe('#isFresh()', () => {
+    context('with a message that HAS NOT been processed', () => {
+      const message = new Message('fresh');
+
+      it('returns true', () => {
+        assert.ok(message.isFresh());
+      });
+    });
+
+    context('with a message that HAS been processed', () => {
+      const message = new Message('not fresh');
+      message._incrementProcessCount();
+
+      it('returns false', () => {
+        assert.ok(!message.isFresh());
+      });
+    });
+  });
+
+  describe('#isUnprocessable()', () => {
+
+    context('with a message that CAN be processed again', () => {
+      const message = new Message('its not dead');
+      it('returns true', () => {
+        assert.ok(!message.isUnprocessable());
+      });
+    });
+
+    context('with a message that CANNOT be processed again', () => {
+      const message = new Message('yep its dead');
+      for(let i = 0; i < 5; i++) {
+        message._incrementProcessCount();
+      }
+      it('returns false', () => {
+        assert.ok(message.isUnprocessable());
+      });
+    });
+  });
+});
